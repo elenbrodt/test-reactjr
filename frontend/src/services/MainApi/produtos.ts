76e2e4af88c06802363bfc2528748e357353bd79
Elen Brodt <elenbrodt@gmail.com>
2023-04-19 14:27:54 -0300

    feat: services MainApi

---
+++ frontend/src/services/MainApi/produtos.ts
@@ -0,0 +1,14 @@
+import baseApi from "./config";
+
+interface ProdutoPayload {
+  descricao: string;
+  preco: string;
+}
+
+export function cadastroProduto(payload: ProdutoPayload) {
+  return baseApi.post("/cadstrar/:id", payload);
+}
+
+export function listarProduto() {
+  return baseApi.get("/");
+}
