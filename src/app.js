3fb786169aad5922af98a7b937476bb792a62888
Elen Brodt <elenbrodt@gmail.com>
2023-04-19 14:06:24 -0300

    build: cors install

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const db = require("./database");
 const routes = require("./routes");
+const cors = require("cors");
+
 const app = express();
 db.hasConection();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log("Servidor conectado"));
+app.listen(3001, () => console.log("Servidor conectado"));
