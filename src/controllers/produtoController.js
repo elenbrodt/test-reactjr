58b3ae2ad864282c8d2dbb49825a48d94e2fcb8b
Elen Brodt <elenbrodt@gmail.com>
2023-04-19 13:41:40 -0300

    feat: delete and update controllers

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -1,4 +1,4 @@
-const Produtos = require("../models/Produtos.js");
+const Produtos = require("../models/produtos.js");
 
 const produtoController = {
   async listarProdutos(req, res) {
@@ -20,6 +20,49 @@ const produtoController = {
       });
 
       res.json("Produto Cadastrado");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async atualizarProduto(req, res) {
+    try {
+      const { id } = req.params;
+      const { descricao, preco, data_cadastro } = req.body;
+      const atualizaProduto = await Produtos.update(
+        {
+          descricao,
+          preco,
+          data_cadastro,
+        },
+        {
+          where: {
+            codigo_id: id,
+          },
+        }
+      );
+      if (atualizaProduto === 0) {
+        res.status(404).json("produto nao encontrado");
+      } else {
+        res.sendStatus(204);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  async deletarProduto(req, res) {
+    try {
+      const { id } = req.params;
+      const deleteProduto = await Produtos.destroy({
+        where: {
+          codigo_id: id,
+        },
+      });
+      if (deleteProduto === 0) {
+        res.status(404).json("produto nao encontrado");
+      } else {
+        res.sendStatus(204);
+      }
     } catch (error) {
       console.log(error);
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const produtoController = require("../controllers/produtoController");
 const routes = express.Router();
 
 routes.get("/", produtoController.listarProdutos);
-
 routes.post("/cadastrar", produtoController.cadastrarProduto);
+routes.delete("/deletar/:id", produtoController.deletarProduto);
+routes.put("/atualizar/:id", produtoController.atualizarProduto);
 
 module.exports = routes;
